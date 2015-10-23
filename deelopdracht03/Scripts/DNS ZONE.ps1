4c1bec6aaf65d3832dbc42187e84107c0a61e938
abdulkadirakbel <abdulkadir.akbel.u1535@student.hogent.be>
2015-10-23 12:08:51 +0200

    no message

---
+++ deelopdracht03/Scripts/DNS ZONE.ps1
@@ -0,0 +1,6 @@
+﻿$domainname = "Project3.be"
+# Reverse lookup zone creëren
+Add-DnsServerPrimaryZone –Name 101.168.192.in-addr.arpa –ReplicationScope Forest
+
+# Primaire zone creëren
+Add-DnsServerPrimaryZone –Name $domainname –ZoneFile $domainname.dns
