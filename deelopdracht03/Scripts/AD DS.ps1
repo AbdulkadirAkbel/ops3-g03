437fd7dac60e5fda65acad07724db41cf4a525d0
abdulkadirakbel <abdulkadir.akbel.u1535@student.hogent.be>
2015-10-23 11:10:49 +0200

    no message

---
+++ deelopdracht03/Scripts/AD DS.ps1
@@ -0,0 +1,16 @@
+﻿Install-windowsfeature AD-domain-services
+Import-Module ADDSDeployment
+$domainname = "Projecten3.be"
+$netbiosName = PROJECTEN3
+Install-ADDSForest 
+    -CreateDnsDelegation:$false `
+    -DatabasePath "C:\Windows\NTDS" `
+    -DomainMode "Win2012R2" `
+    -DomainName $domainname `
+    -DomainNetbiosName $netbiosName `
+    -ForestMode "Win2012R2" `
+    -InstallDns:$true `
+    -LogPath "C:\Windows\NTDS" `
+    -NoRebootOnCompletion:$false `
+    -SysvolPath "C:\Windows\SYSVOL" `
+    -Force:$true
