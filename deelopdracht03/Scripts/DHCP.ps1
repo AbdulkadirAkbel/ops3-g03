7aafa27bf8ad0aeb9400b1dd7fe5496d944f9599
AbdulkadirAkbel <abdulkadir.akbel.u1535@student.hogent.be>
2015-10-30 11:10:42 +0100

    Update DHCP.ps1

--- deelopdracht03/Scripts/DHCP.ps1
+++ deelopdracht03/Scripts/DHCP.ps1
@@ -1,4 +1,3 @@
-﻿$domain = "Projecten3.be"
 
 # Installeer DHCP en management tools
 Install-WindowsFeature DHCP -IncludeManagementTools
@@ -7,10 +6,10 @@ Install-WindowsFeature DHCP -IncludeManagementTools
 Add-DhcpServerv4Scope -Name "Werkstations" -StartRange 192.168.101.30 -EndRange 192.168.101.200 -SubnetMask 255.255.255.0
 
 # DHCP opties
-Set-DhcpServerv4OptionValue -DnsDomain $domain -DnsServer 192.168.101.11
+Set-DhcpServerv4OptionValue -DnsDomain Project3.be -DnsServer 192.168.101.11
 
 # DHCP activeren
-Add-DhcpServerInDC -DnsName PROJECTEN3.$domain
+Add-DhcpServerInDC -DnsName PROJECTEN3.Project3.be
 
 # DHCP exclusions toevoegen
 Add-DhcpServerv4ExclusionRange –ScopeId 192.168.101.0 –StartRange 192.168.101.1 –EndRange 192.168.101.29
